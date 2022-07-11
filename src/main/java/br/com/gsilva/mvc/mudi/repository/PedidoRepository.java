b0afa6a99fe68d81f87085965e46e7d72fa0eca2
Gabriel-Silva-Oliveira <gabriel.silva.oliveira@kyndryl.com>
2022-07-10 22:00:03 -0300

    first commit

---
+++ src/main/java/br/com/gsilva/mvc/mudi/repository/PedidoRepository.java
@@ -0,0 +1,29 @@
+package br.com.gsilva.mvc.mudi.repository;
+
+import java.util.List;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import br.com.gsilva.mvc.mudi.model.Pedido;
+import br.com.gsilva.mvc.mudi.model.StatusPedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long>{
+
+	@Cacheable("pedidos")
+	List<Pedido> findByStatus(StatusPedido status, Pageable paginacao);
+
+	@Query("SELECT p FROM Pedido p JOIN p.user u WHERE u.username = :username")
+	List<Pedido> findAllByUsuario(@Param("username") String username);
+
+	@Query("SELECT p FROM Pedido p JOIN p.user u WHERE u.username = :username and p.status = :status")
+	List<Pedido> findByStatusEUsuario(@Param("status") StatusPedido status, @Param("username") String username);
+	
+}
